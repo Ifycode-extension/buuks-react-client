ac69fd19d236b8c8114e4578f95875591e579f5d
Ifycode <marychord@gmail.com>
2022-03-17 22:52:17 +0100

    Add form and POST fetch request for login

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,11 +1,45 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 const Login = (): JSX.Element => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const loginUser = async (e: any): Promise<void> => {
+    e.preventDefault();
+    const response = await fetch(`http://localhost:8000/auth/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email,
+        password
+      })
+    });
+
+    const data = await response.json();
+    console.log(data);
+  }
+
   return (
     <Fragment>
       <section>
         <h1>Login page!</h1>
-        <p>Login form goes here</p>
+        <form onSubmit={loginUser}>
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button>Login</button>
+        </form>
       </section>
     </Fragment>
   );
