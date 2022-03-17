8dd98d1c9c2fb09e49a0c37ef8f9ccc40f183531
Ifycode <marychord@gmail.com>
2022-03-17 22:22:46 +0100

    Add form and POST fetch request for signup

--- src/components/pages/SignUp.tsx
+++ src/components/pages/SignUp.tsx
@@ -1,11 +1,61 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 const SignUp = (): JSX.Element => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  const [name, setName] = useState('');
+
+  const signupUser = async (e: any): Promise<void> => {
+    e.preventDefault();
+    const response = await fetch(`http://localhost:8000/users/signup`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email,
+        password,
+        passwordConfirmation,
+        name,
+      })
+    });
+
+    const data = await response.json();
+    console.log(data);
+  }
+
   return (
     <Fragment>
       <section>
         <h1>Signup page!</h1>
-        <p>Signup form goes here</p>
+        <form onSubmit={signupUser}>
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Confirm password"
+            value={passwordConfirmation}
+            onChange={(e) => setPasswordConfirmation(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button>Signup</button>
+        </form>
       </section>
     </Fragment>
   );
