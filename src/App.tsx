7d80b822fa6a144e6b162376dc6b4e2cdea42c1b
Ifycode <marychord@gmail.com>
2022-03-19 16:04:14 +0100

    Hide header in books page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import { Fragment } from 'react';
+import { useLocation } from 'react-router-dom';
 
 // TODO: Prevent NOT logged in user from Accessing /books page (through changing browser url)
 
 const App = (): JSX.Element => {
+  const location = useLocation();
+  const booksPage = location.pathname === '/books';
   return (
     <Fragment>
-      <Header />
+      {!booksPage && <Header />}
       <Outlet />
     </Fragment>
   );
