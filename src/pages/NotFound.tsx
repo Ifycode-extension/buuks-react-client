f345d3b72643fe4f4a515c5206a694039222c412
Ifycode <marychord@gmail.com>
2022-03-23 00:18:55 +0100

    Remove unwanted fragment element

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,14 +1,12 @@
-import { Fragment, ReactElement } from 'react';
+import { ReactElement } from 'react';
 import { Link } from 'react-router-dom';
 
 const NotFound = (): ReactElement => {
   return (
-    <Fragment>
-      <section>
-        <h1>Page not found - 404</h1>
-        <Link to="/" className="text-pink-800 underline">Back to home page</Link>
-      </section>
-    </Fragment>
+    <section>
+      <h1>Page not found - 404</h1>
+      <Link to="/" className="text-pink-800 underline">Back to home page</Link>
+    </section>
   );
 }
 
