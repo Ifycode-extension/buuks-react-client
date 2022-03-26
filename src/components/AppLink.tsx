ab8420a8752e828a993d08ec43e6a7e7f5d84543
Ifycode <marychord@gmail.com>
2022-03-26 05:50:32 +0100

    Extract appLinks into it's own component

---
+++ src/components/AppLink.tsx
@@ -0,0 +1,12 @@
+import { ReactElement } from "react";
+import { Link } from "react-router-dom";
+import { AuthContainer } from "../hooks/useAuth";
+
+const AppLink = ({ to, tailwindStyle, text }: { to: string, tailwindStyle: string, text: string }): ReactElement => {
+  const auth = AuthContainer.useContainer();
+  return (
+    <Link to={to} className={tailwindStyle}>{text}</Link>
+  );
+}
+
+export default AppLink;
